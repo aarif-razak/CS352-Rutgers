4008b4c038386d259c6eb3229e5438e91eb6e3e7
OrderedChaos-Dev <modding.jt9@gmail.com>
2021-02-06 18:14:03 -0500

    finished

--- project1/client.py
+++ project1/client.py
@@ -12,16 +12,15 @@ def client():
     except socket.error as err:
         print('socket open error: {} \n'.format(err))
         exit()
-        
-    # Define the port on which you want to connect to the server
-    port = 50007
-    localhost_addr = socket.gethostbyname(socket.gethostname())
 
     #Inputs
     rsHostname, rsListenPort, tsListenPort = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+    rs_ip = socket.gethostbyname(socket.gethostname())
+    tsSocket = None
+    ts_ip = None
 
     # connect to the server on local machine
-    server_binding = (rsHostname, rsListenPort)
+    server_binding = (rs_ip, rsListenPort)
     cs.connect(server_binding)
 
     file = open('PROJI-HNS.txt', 'r')
@@ -33,32 +32,36 @@ def client():
         #Connect to the new server, send a line                
         msg = hname
         cs.send(msg.encode('utf-8'))
+        print('Sent {} to RS'.format(msg))
         #Get the response and write it to the output file
         # Receive data from the server
         data_from_server = cs.recv(100).decode('utf-8')
         data = data_from_server.split(' ')
+        print('Received {} from RS'.format(data_from_server))
         if(data[2] == 'A'):
-            outputFile.write("{}".format(data_from_server))
+            outputFile.write("{}\n".format(data_from_server))
         elif(data[2] == 'NS'):
-            #Connect to TS (new socket)
             try:
-                tsSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                print("[C]: TSClient socket created")
+                # Connect to TS (new socket)
+                if tsSocket is None:
+                    tsSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    ts_ip = socket.gethostbyname(socket.gethostname())
+                    tsConnect = (ts_ip, tsListenPort)
+                    tsSocket.connect(tsConnect)
+                    print("[C]: TSClient socket created")
             except socket.error as err:
                 print('TSsocket open error: {} \n'.format(err))
                 exit()
-            tsConnect = (data, tsListenPort)
-            tsSocket.connect(tsConnect)
+
             tsSocket.send(msg.encode('utf-8'))
-
-            data_from_server = tsSocket.recv(100)
-
-            outputFile.write("{}".format(data_from_server))
-
-            tsSocket.close()
+            print('Sent {} to TS'.format(msg))
+            data_from_server = tsSocket.recv(100).decode()
+            print('Received {} from TS'.format(data_from_server))
+            outputFile.write("{}\n".format(data_from_server))
             
     #Close client socket
     cs.close()
+    tsSocket.close()
     exit()
 
 client()
---
+++ project1/ts.py
@@ -0,0 +1,57 @@
+import threading
+import sys
+import socket
+
+def server(listenPort):
+    # read from PROJI-DNSRS.txt and convert contents into a list of 3-tuples
+    file = open('PROJI-DNSTS.txt', 'r')
+    contents = file.read().splitlines()
+    file.close()
+    hosts = []
+    for x in contents:
+        line = x.split(' ')
+        hosts.append(tuple(line))
+    try:
+        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[S]: Server socket created")
+    except socket.error as err:
+        print('socket open error: {}\n'.format(err))
+        exit()
+
+    server_binding = ('', listenPort)
+    ss.bind(server_binding)
+    ss.listen(1)
+    host = socket.gethostname()
+    print("[S]: Server host name is {}".format(host))
+    localhost_ip = (socket.gethostbyname(host))
+    print("[S]: Server IP address is {}".format(localhost_ip))
+    csockid, addr = ss.accept()
+    print ("[S]: Got a connection request from a client at {}".format(addr))
+
+    try:
+        while 1:
+            msg = csockid.recv(100).decode()
+            if not msg:
+                print("No more messages received. Closing connection with {}".format(addr))
+                break
+            print("Received message: {}".format(msg))
+            result = [(x, y, z) for x, y, z in hosts if msg.lower() == x.lower()]
+            response = ''
+            if len(result) > 0:
+                response = "{} {} {}".format(result[0][0], result[0][1], result[0][2])
+                csockid.send(response.encode('utf-8'))
+            else:
+                response = "{} - Error:HOST NOT FOUND".format(msg)
+                csockid.send(response.encode('utf-8'))
+            print("Sent message: {}".format(response))
+    except:
+        print('Connection closed by client.')
+    # Close the server socket
+    ss.close()
+    exit()
+
+
+if __name__ == "__main__":
+    listenPort = int(sys.argv[1])
+    t1 = threading.Thread(name='server', target=server, args=[listenPort])
+    t1.start()
